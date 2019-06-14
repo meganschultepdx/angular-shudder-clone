4f20c72fc5194e20eec56de0fb520884183bae65
megan schulte <megan.schulte.pdx@gmail.com>
2019-06-14 10:07:35 -0700

    add drop down menu to nav bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NavBarComponent } from './navbar/navbar.component';
 import { AccountCreationComponent } from './account-creation/account-creation.component';
 import { LoginComponent } from './login/login.component';
 
