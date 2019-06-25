6ad10f0dde6d78be944e155ff9c6778e711bd22c
megan schulte <megan.schulte.pdx@gmail.com>
2019-06-24 22:07:27 -0700

    change horror101 back to be second list in Firebase

--- src/app/collection.service.ts
+++ src/app/collection.service.ts
@@ -1,19 +1,26 @@
 import { Injectable } from '@angular/core';
 import { Collection } from './collection.model';
+import { Horror101 } from './horror101.model';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Injectable()
 
 export class CollectionService {
   collections: FirebaseListObservable<any[]>;
+  horror101s: FirebaseListObservable<any[]>;
 
   constructor(private database: AngularFireDatabase) {
     this.collections = database.list('collections');
+    this.horror101s = database.list('horror101s');
   }
 
 
   getCollections() {
     return this.collections;
+  }
+
+  getHorror101s() {
+    return this.getHorror101s;
   }
 
 }
---
+++ src/app/horror101.model.ts
@@ -0,0 +1,3 @@
+export class Horror101 {
+  constructor (public count: string, public title: string, public description: string) {}
+}
